27eb0c14a8ee77d6e358e866355eed331e895a39
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2022-12-06 09:58:42 -0800

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,74 @@
+
+
+import java.util.*;
+import java.io.*;
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                    
+                }
+            }
+        }
+    }
+    public static void swap(int arr[],int i , int j){
+        int temp = arr[i];
+        arr[i]= arr[j];
+        arr[j]= temp;
+    }
+}
+
+
+class Sorting
+{
+    //method to print the Elements of the array
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		while(t>0)
+		{
+		    //taking total elements
+			int n = sc.nextInt();
+			
+			//creating a new array of length n
+			int arr[] = new int[n];
+			
+			//inserting elements to the array
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt(); 
+			}
+			
+			
+			//calling bubbleSort() method
+			new Solution().bubbleSort(arr,n);
+			
+			//calling printArray() method
+			printArray(arr);
+			
+		t--;	
+		}
+	}
+
+}
